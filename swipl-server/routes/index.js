27625017286acd548ca5bcd82ba5481fea1c7b51
Marc Hanheide <marc@hanheide.net>
2014-01-03 14:43:28 +0000

    now interacting with prolog!

--- swipl-server/routes/index.js
+++ swipl-server/routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: req.session.lastentry });
+  res.render('index', { title: 'Express' });
 };
--- swipl-server/routes/userlogin.js
+++ swipl-server/routes/userlogin.js
@@ -5,6 +5,17 @@
 
 exports.list = function(req, res){
   console.log(req.body);
-  req.session.lastentry=req.body.email;
-  res.end();
+  var m = swipl.module("myown");
+  var ret = '';
+  r = m.assert(req.body.email);
+  q = m.open_query("red", ["Result"]);
+  r = q.next_solution();
+  while( r ) {
+	ret = ret + '<tr><td>' + r.Result+'</td></tr>';
+	r = q.next_solution();
+  }
+  q.close();
+	console.log(ret);
+  res.type('html');
+  res.end('<table>'+ret+'</table>');
 };
